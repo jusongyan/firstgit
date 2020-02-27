1a2f533f0a7002c7aeb810261d394807fe821dbe
jusongyan <jusongyan@163.com>
2020-02-27 10:37:52 +0800

    删除bpc-sms

--- bpc-sms/bpc-sms/stop.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-
-PID=$(ps -fu `whoami`|grep BPC-SMS-01|grep -v grep|awk '{print $2}')
-
-if [ x$PID != x ] ;then
-	kill -9 $PID
-fi
--- bpc-sms/src/main/java/com/ect/nrac/cmc/sms/esb/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- * @author wucg
- * @since 2018-3-8
- * 
- */
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.ect888.com/service/MessageService", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.ect.nrac.cmc.sms.esb;
